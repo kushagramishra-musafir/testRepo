03c649f85a058059194dc08b36db4f2176403eb4
kushagramishra-musafir <kushagra.mishra@in.musafir.com>
2025-09-30 12:03:58 +0530

    stage ci cd test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> 123</h1>
+      <h1>this is stage build for test server</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
